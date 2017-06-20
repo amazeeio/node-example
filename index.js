69a2a2832d05c027da22027ce8dd54bd3c3524c2
Michael Schmid <michael@amazee.com>
2017-06-20 11:06:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World! This is a demo.')
 })
 
 app.listen(3000, function () {
