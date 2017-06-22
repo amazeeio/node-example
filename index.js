ebac25fd1b2e38f96761c6f9f7a50b7733d24f3e
Schnitzel <michael@amazee.com>
2017-06-22 11:14:36 -0500

    Hello from Austin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World! This is a demo. Moar Demo! Hi!')
+  res.send('Hello World! This is a demo. Moar Demo! Hi! Hello from Austin!')
 })
 
 app.listen(3000, function () {
