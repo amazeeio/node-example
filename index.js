c4fc5979a45ab272a46bdcc36e1b23d08b4ce16d
Schnitzel <michael@amazee.com>
2017-06-22 10:56:48 -0500

    docker-compose in demo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World! This is a demo. Moar Demo!')
+  res.send('Hello World! This is a demo. Moar Demo! Hi!')
 })
 
 app.listen(3000, function () {
