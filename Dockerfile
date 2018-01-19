698ad5bad7ec404159f98215f8ab95d56dd421a1
Schnitzel <michael@amazee.com>
2018-01-19 13:30:39 +0100

    update to newest lagoon

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM amazeeio/centos7-node-builder:8 as builder
+ARG IMAGE_REPO
+FROM ${IMAGE_REPO:-lagoon}/node:8-builder as builder
 COPY package.json yarn.lock /app/
 RUN yarn install
 
-FROM amazeeio/centos7-node:8
+FROM ${IMAGE_REPO:-lagoon}/node:8
 COPY --from=builder /app/node_modules /app/node_modules
 COPY . /app/
 
 EXPOSE 3000
 
-CMD yarn start
+CMD ["yarn", "run", "start"]
--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const app = express()
 app.get('/', function (req, res) {
   let result = []
   Object.keys(process.env).map(key => {
-    result.push(`${key}: ${process.env[key]}`)
+    result.push(`${key}=${process.env[key]}`)
   })
   result.sort()
-  result.unshift(`Hello World! This is a demo. <br /><br />`)
 
   res.send(result.join("<br />"))
 })
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
