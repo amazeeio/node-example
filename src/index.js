4ac7e57775774ec36ddd217d377514800d19604c
Bryan Gruneberg <hello@bryangruneberg.com>
2023-02-27 11:26:28 -0800

    Make it a bit dynamic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
 
 app.get('/', function (req, res) {
-  let result = []
-  result.push(`<br /><pre>
+
+  let words = "";
+  try {
+    words = fs.readFileSync('src/words.txt', 'utf8');
+    console.log(words);
+  } catch (err) {
+    console.error(err);
+    words = `
    █████  ███    ███  █████  ███████ ███████ ███████    ██  ██████      ██████   ██████   ██████ ██   ██ ███████     ██    ██  ██████  ██    ██
   ██   ██ ████  ████ ██   ██    ███  ██      ██         ██ ██    ██     ██   ██ ██    ██ ██      ██  ██  ██           ██  ██  ██    ██ ██    ██
   ███████ ██ ████ ██ ███████   ███   █████   █████      ██ ██    ██     ██████  ██    ██ ██      █████   ███████       ████   ██    ██ ██    ██
   ██   ██ ██  ██  ██ ██   ██  ███    ██      ██         ██ ██    ██     ██   ██ ██    ██ ██      ██  ██       ██        ██    ██    ██ ██    ██
   ██   ██ ██      ██ ██   ██ ███████ ███████ ███████ ██ ██  ██████      ██   ██  ██████   ██████ ██   ██ ███████        ██     ██████   ██████
-</pre><br />
-  `)
+  `;
+  }
+
+  let result = []
+  result.push(`<br /><pre>`);
+  result.push(words);
+  result.push(`</pre><br />`);
   res.header('X-LAGOON' , process.env.HOSTNAME )
   res.send(result.join("<br />"))
 })
